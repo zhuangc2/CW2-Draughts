5788533bfe937c0225aa1a2a5f2aa298fd3f7db6
Cynthia Zhuang <c.zhuang@lancaster.ac.uk>
2019-03-20 10:28:38 +0000

    task5 in progress, rules incomleted

--- Board.java
+++ Board.java
@@ -1,16 +1,19 @@
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.util.ArrayList;
+import java.awt.event.*;
 
-public class Board extends JPanel implements ActionListener{
+public class Board extends JPanel implements ActionListener, MouseListener{
     GridLayout grid = new GridLayout(8,8);
     private Square squareArr[][] = new Square[8][8];
+
+    private Square currClicked = null;
+	private ArrayList<Square> ValideMoves = new ArrayList<>();
 
     public Board(){
         super();
         createAndShowGUI();
+
         JFrame frame = new JFrame("Draughts");
         frame.setContentPane(this);
 		frame.setResizable(false);
@@ -24,6 +27,9 @@ public class Board extends JPanel implements ActionListener{
         setLayout(grid);
 
         addWhiteChecker();
+        addRedChecker();
+        addMouseListener(this);
+        
     }
     private void initialiseSquareArr(){
         for(int i = 0; i < 8; i++)						
@@ -78,10 +84,6 @@ public class Board extends JPanel implements ActionListener{
 
     }
 
-    public void actionPerformed(ActionEvent actionEvent) {
-
-    }
-
     public Dimension getPreferredSize() {
 		return new Dimension(640, 640);
     }
@@ -97,6 +99,120 @@ public class Board extends JPanel implements ActionListener{
                 }
             }
         }
+    }
+
+    public void addRedChecker(){
+        for (int i = 0; i < 3; i++)
+        {
+            for (int j = 0; j < 8; j++)
+            {
+                if (squareArr[i][j].getSquareType() == SquareType.White){
+                    squareArr[i][j].setIcon(new ImageIcon(Source.RedChecker));
+                    squareArr[i][j].setCheckerType(CheckerType.Red);
+                }
+            }
+        }
+    }
+    
+    // private Square currClicked = null;
+    // private ArrayList<Square> ValideMoves = new ArrayList<>();
+
+    public void actionPerformed(ActionEvent actionEvent) {
+		Object src = actionEvent.getSource();
+		if ( !(src instanceof Square) ) return;
+
+		if (currClicked == null){
+            if (((Square) src).getCheckerType() == CheckerType.None) 
+                return;
+			currClicked = (Square) src;
+			System.out.println("Current Clicked " + ((Square) src).getSquareX() + " " + ((Square) src).getSquareY());
+            highlightSquare();
+            return;
+		}
+
+		if (!ValideMoves.contains(src)) return;
+
+		currClicked.moveTo(((Square) src));
+		System.out.println("Move to " + ((Square) src).getSquareX() + " " + ((Square) src).getSquareY());
+		currClicked = null;
+        cancelHighlight();
+        repaint();
+
+    }
+
+    private void highlightSquare(){
+		for (int i = currClicked.getSquareX() - 2; i <= currClicked.getSquareX() + 2; i++){
+			if (i < 0 || i > 7) continue;
+
+			for (int j = currClicked.getSquareY() -2; j <= currClicked.getSquareY() + 2; j++){
+				if (j < 0 || j > 7) continue;
+
+				if (currClicked.canMoveTo(squareArr[i][j])){
+                    squareArr[i][j].setIcon(new ImageIcon(Source.Selected));
+					squareArr[i][j].setHighlight(true);
+					ValideMoves.add(squareArr[i][j]);
+				}
+			}
+		}
+    }
+    
+    private void cancelHighlight(){
+
+    	for (Square square : ValideMoves){
+			if (square.isHighlight()){
+				square.setHighlight(false);
+                if (square.getCheckerType() == CheckerType.None)
+                {
+					switch (square.getSquareType()){
+						case White: square.setIcon(new ImageIcon(Source.WhiteSquare)); break;
+						case Black: square.setIcon(new ImageIcon(Source.BlackSquare)); break;
+						default: break;
+					}
+                } 
+                else 
+                {
+					switch (square.getCheckerType()){
+						case White: square.setIcon(new ImageIcon(Source.WhiteChecker)); break;
+						case Red: square.setIcon(new ImageIcon(Source.RedChecker)); break;
+						case WhiteKing: square.setIcon(new ImageIcon(Source.WhiteKing)); break;
+						case RedKing: square.setIcon(new ImageIcon(Source.RedKing)); break;
+						default: break;
+					}
+				}
+			}
+		}
+
+    	ValideMoves.clear();
+	}
+
+    private int checkCheckerType(CheckerType checkerType){
+        switch (checkerType){
+            case Red:
+            case RedKing: return 1;
+            case White:
+            case WhiteKing: return 2;
+            default: return 0;
+        }
+    }
+
+	public void mousePressed(MouseEvent mouseEvent) {
+
+	}
+
+	public void mouseReleased(MouseEvent mouseEvent) {
+
+	}
+
+    public void mouseClicked(MouseEvent mouseEvent) {
+
+	}
+
+	public void mouseEntered(MouseEvent mouseEvent) {
+
+	}
+
+	public void mouseExited(MouseEvent mouseEvent) {
+
     }
 
 
--- Square.java
+++ Square.java
@@ -7,9 +7,17 @@ public class Square extends JButton{
     private int squareY;
     private SquareType squareType;
     private CheckerType checkerType;
+    private boolean Highlight = false;
 
     public Square(int x, int y){
-        
+        this(x, y, CheckerType.None);
+    }
+
+    public Square(int x, int y, CheckerType checkerType){
+        super();
+        squareX = x;
+        squareY = y;
+        this.checkerType = checkerType;
     }
 
     public int getSquareX(){
@@ -36,5 +44,100 @@ public class Square extends JButton{
 
 	public void setCheckerType(CheckerType checkerType) {
 		this.checkerType = checkerType;
+    }
+
+    public boolean isHighlight() {
+		return Highlight;
 	}
+
+	public void setHighlight(boolean highlight) {
+		Highlight = highlight;
+	}
+
+    public void moveTo(Square square){
+        if (!canMoveTo(square)) return;
+
+        square.setCheckerType(getCheckerType());
+        setCheckerType(CheckerType.None);
+        setIcon(new ImageIcon(Source.WhiteSquare));
+
+        setToKing(square);
+
+    	switch (square.getCheckerType()){
+			case White: square.setIcon(new ImageIcon(Source.WhiteKing)); break;
+			case Red: square.setIcon(new ImageIcon(Source.RedKing)); break;
+			case WhiteKing: square.setIcon(new ImageIcon(Source.WhiteKing)); break;
+			case RedKing: square.setIcon(new ImageIcon(Source.RedKing)); break;
+			default: break;
+		}
+    }
+
+    //decide whether the checker can move to valid square
+    public boolean canMoveTo(Square square){
+    	if (square.getCheckerType() != CheckerType.None) return false;
+    	switch (getCheckerType()){
+            //White checkers can only move to square where xAxis-1 and yAxis+1 or xAxis-1 and yAxis-1
+			case White:
+				if ( getSquareX() - 1 == square.getSquareX()) {
+					if ((getSquareY() + 1 == square.getSquareY()) || (getSquareY() - 1 == square.getSquareY()))
+						return true; }
+
+				if ( getSquareX() - 2 == square.getSquareX()) {
+					if ((getSquareY() + 2 == square.getSquareY()) || (getSquareY() - 2 == square.getSquareY()))
+						return true; }
+
+                return false;
+                
+            case Red:
+                if ( getSquareX() + 1 == square.getSquareX()) {
+                    if ((getSquareY() + 1 == square.getSquareY()) || (getSquareY() - 1 == square.getSquareY()))
+                    return true; }
+
+                if ( getSquareX() + 2 == square.getSquareX()) {
+                    if ((getSquareY() + 2 == square.getSquareY()) || (getSquareY() - 2 == square.getSquareY()))
+                    return true; }
+                return false;
+
+            case WhiteKing:
+                if ( getSquareX() + 1 == square.getSquareX() || (getSquareX() - 1 == square.getSquareX())) {
+                    if ((getSquareY() + 1 == square.getSquareY()) || (getSquareY() - 1 == square.getSquareY()))
+                        return true; }
+
+                if ( getSquareX() + 2 == square.getSquareX() || (getSquareX() - 2 == square.getSquareX())) {
+                    if ((getSquareY() + 2 == square.getSquareY()) || (getSquareY() - 2 == square.getSquareY()))
+                        return true; }
+                return false;
+            
+            
+            case RedKing:
+            default: return false;
+		}
+
+    }
+
+    private void setToKing(Square square){
+
+    	switch (square.getCheckerType()){
+
+    		case RedKing: return;
+			case WhiteKing: return;
+
+			case Red:
+				if (square.getSquareX() == 7){
+					square.setCheckerType(CheckerType.RedKing);
+					System.out.println("Red King"); }
+				break;
+
+			case White:
+				if (square.getSquareX() == 0){
+					square.setCheckerType(CheckerType.WhiteKing);
+					System.out.println("White King"); }
+				break;
+
+			default: break;
+		}
+
+    }
+
+    
 }
